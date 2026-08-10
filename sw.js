094b008782f620538919418a04ec8a58eacd705e
jose99-lab <raherimananajose9@gmail.com>
2026-08-10 08:01:13 +0300

    Change CACHE_NAME from 'sillon-v2' to 'sillon-v3'
    Updated CACHE_NAME to reflect version change.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sillon-v2'; // ovay ihany koa satria misy fanovana APP_SHELL
+const CACHE_NAME = 'sillon-v3'; // ovay ihany koa satria misy fanovana APP_SHELL
 const APP_SHELL = [
   './index.html',
   './manifest.json',
