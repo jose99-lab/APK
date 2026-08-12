0c7fa216b78101ba251b1f2186d9e4584f93cb06
jose99-lab <raherimananajose9@gmail.com>
2026-08-12 07:58:13 +0300

    Increment CACHE_NAME to 'sillon-v29'
    Updated CACHE_NAME to version 29 for app shell changes.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sillon-v28'; // ovay ihany koa satria misy fanovana APP_SHELL
+const CACHE_NAME = 'sillon-v29'; // ovay ihany koa satria misy fanovana APP_SHELL
 const APP_SHELL = [
   './index.html',
   './manifest.json',
